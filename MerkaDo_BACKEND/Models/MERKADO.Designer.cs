de7cc16a5050568e7a55842c67103f6f0ed1bf32
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-06-28 18:35:33 -0400

    commit

--- MerkaDo_BACKEND/Models/MERKADO.Designer.cs
+++ MerkaDo_BACKEND/Models/MERKADO.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Backup de dulce\1-EXTRA\Merka.do\PROJECT\MerkaDo_BACKEND\MerkaDo_BACKEND\Models\MERKADO.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Blast\OneDrive\Desktop\merka.do\MerkaDo_BACKEND\MerkaDo_BACKEND\Models\MERKADO.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
